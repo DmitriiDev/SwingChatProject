5b6c3fed9cbb602e25b3324fc7efb55681e71d98
dmitrii.vokuev <dmitrii.vokuev@smaato.com>
2018-05-24 10:07:45 +0200

    fix size textField and textArea,fix send  emprty message

--- src/Example.java
+++ src/Example.java
@@ -1,6 +1,6 @@
 import com.sun.tools.javac.comp.Enter;
 
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
@@ -16,29 +16,30 @@ public class Example extends JFrame  {
         ImageIcon img = new ImageIcon("java.png");
         window.setIconImage(img.getImage());
 
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
         JTextField textField = new JTextField(1);
-        textField.setBackground(Color.WHITE);
+        textField.setMinimumSize(new Dimension(50, 25));
+        textField.setPreferredSize(new Dimension(50, 25));
+        textField.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
         textField.getDocument().putProperty("filterNewlines", Boolean.TRUE);
 
 
         JTextArea textArea = new JTextArea();
-        textArea.setBackground(Color.white);
         textArea.setEditable(false);
+        textArea.setPreferredSize(new Dimension(500,500));
 
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
 
         JButton sendButton = new JButton("Send");
-        sendButton.setVerticalTextPosition(AbstractButton.CENTER);
-        sendButton.setHorizontalTextPosition(AbstractButton.RIGHT);
         textField.addKeyListener
                 (new KeyAdapter() {
                      public void keyPressed(KeyEvent e) {
                          int key = e.getKeyCode();
                          if (key == KeyEvent.VK_ENTER) {
-                             String text = textField.getText();
+                             String text = textField.getText().trim();
                              if (text.length() > 0) {
                                  textArea.append(text + newline);
                                  textField.selectAll();
@@ -53,7 +54,7 @@ public class Example extends JFrame  {
 
         sendButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String text = textField.getText();
+                String text = textField.getText().trim();
                 if (text.length() > 0) {
                     textArea.append(text + newline);
                     textField.selectAll();
@@ -65,17 +66,23 @@ public class Example extends JFrame  {
         });
 
 
-        panel.add(textArea);
-        panel.add(textField);
-        panel.add(sendButton);
+
+        panel.add(textArea,Component.RIGHT_ALIGNMENT);
+        panel.add(textField,Component.RIGHT_ALIGNMENT);
+        panel.add(sendButton, Component.RIGHT_ALIGNMENT);
+
+
+
 
 
         window.getContentPane().add(panel);
-        window.pack();
+
 
         window.setLocationRelativeTo(null);
+        window.pack();
         window.setVisible(true);
-        window.setSize(300, 200);
+
+
         textField.requestFocusInWindow();
 
     }
